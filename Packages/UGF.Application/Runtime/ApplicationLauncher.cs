bafb9ccaf9ffd2130581734cac2e5954a0e740fc
Alexander <vorobyovvorobyov@gmail.com>
2019-08-29 22:35:27 +0300

    Add summaries

--- Packages/UGF.Application/Runtime/ApplicationLauncher.cs
+++ Packages/UGF.Application/Runtime/ApplicationLauncher.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 
 namespace UGF.Application.Runtime
 {
+    /// <summary>
+    /// Represents a <see cref="MonoBehaviour"/> that create and initialize application.
+    /// </summary>
     public abstract class ApplicationLauncher : MonoBehaviour
     {
         private InitializeState m_launchState = new InitializeState();
@@ -15,6 +18,11 @@ namespace UGF.Application.Runtime
             yield return Launch();
         }
 
+        /// <summary>
+        /// Launch application creation and initialization.
+        /// <para>This method called from start, if component is active and enabled.</para>
+        /// <para>This method can be called only once.</para>
+        /// </summary>
         public IEnumerator Launch()
         {
             m_launchState.Initialize();
@@ -37,22 +45,39 @@ namespace UGF.Application.Runtime
             OnLaunched(application);
         }
 
+        /// <summary>
+        /// Invoked right at the start of the launch.
+        /// </summary>
         protected virtual void OnLaunch()
         {
         }
 
+        /// <summary>
+        /// Invoked before application creation.
+        /// </summary>
         protected virtual IEnumerator PreloadResourcesAsync()
         {
             yield break;
         }
 
+        /// <summary>
+        /// Invoked after resources preload and ready to create application.
+        /// </summary>
         protected abstract IApplication CreateApplication();
 
+        /// <summary>
+        /// Invoked after application creation.
+        /// </summary>
+        /// <param name="application">The application.</param>
         protected virtual void InitializeApplication(IApplication application)
         {
             application.Initialize();
         }
 
+        /// <summary>
+        /// Invoked after application and modules initialized.
+        /// </summary>
+        /// <param name="application">The application.</param>
         protected virtual IEnumerator InitializeModulesAsync(IApplication application)
         {
             foreach (KeyValuePair<Type, IApplicationModule> pair in application.Modules)
@@ -64,6 +89,10 @@ namespace UGF.Application.Runtime
             }
         }
 
+        /// <summary>
+        /// Invoked after all launch completed.
+        /// </summary>
+        /// <param name="application">The application.</param>
         protected virtual void OnLaunched(IApplication application)
         {
         }
--- Packages/UGF.Application/Runtime/ApplicationUnityLauncher.cs
+++ Packages/UGF.Application/Runtime/ApplicationUnityLauncher.cs
@@ -2,12 +2,22 @@ using UnityEngine;
 
 namespace UGF.Application.Runtime
 {
+    /// <summary>
+    /// Represents <see cref="ApplicationUnity"/> launcher.
+    /// </summary>
     public class ApplicationUnityLauncher : ApplicationLauncher
     {
         [SerializeField] private bool m_uninitializeOnUnityQuitting = true;
         [SerializeField] private bool m_provideStaticInstance = true;
 
+        /// <summary>
+        /// Gets or sets the value that determines whether application subscribed to Unity application quitting event to uninitialize it self.
+        /// </summary>
         public bool UninitializeOnUnityQuitting { get { return m_uninitializeOnUnityQuitting; } set { m_uninitializeOnUnityQuitting = value; } }
+
+        /// <summary>
+        /// Gets or sets the value that determines whether application provide static instance via <see cref="ApplicationInstance"/>.
+        /// </summary>
         public bool ProvideStaticInstance { get { return m_provideStaticInstance; } set { m_provideStaticInstance = value; } }
 
         protected override IApplication CreateApplication()
