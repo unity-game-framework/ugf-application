62ba7942ecbdb353a291d9aa73764c3ef1db64e6
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-01 22:49:29 +0300

    Change components add menu

--- Packages/UGF.Application/Runtime/ApplicationLauncherResources.cs
+++ Packages/UGF.Application/Runtime/ApplicationLauncherResources.cs
@@ -6,6 +6,7 @@ using Object = UnityEngine.Object;
 
 namespace UGF.Application.Runtime
 {
+    [AddComponentMenu("Unity Game Framework/Application/Application Launcher Resources", 2000)]
     public class ApplicationLauncherResources : ApplicationLauncherResourceLoader
     {
         [SerializeField] private List<Object> m_resources = new List<Object>();
