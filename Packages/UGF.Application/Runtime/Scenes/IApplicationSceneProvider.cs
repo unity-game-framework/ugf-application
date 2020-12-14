679173c9f3238b6f6718fc89d64abadc1bba6461
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-14 21:42:10 +0300

    Add ApplicationSceneAccess

---
+++ Packages/UGF.Application/Runtime/Scenes/IApplicationSceneProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace UGF.Application.Runtime.Scenes
+{
+    public interface IApplicationSceneProvider
+    {
+        IReadOnlyDictionary<Scene, IApplication> Applications { get; }
+
+        event ApplicationSceneHandler Added;
+        event ApplicationSceneHandler Removed;
+        event Action Cleared;
+
+        void Add(Scene scene, IApplication application);
+        bool Remove(Scene scene);
+        void Clear();
+        T Get<T>(Scene scene) where T : class, IApplication;
+        IApplication Get(Scene scene);
+        bool TryGet<T>(Scene scene, out T application) where T : class, IApplication;
+        bool TryGet(Scene scene, out IApplication application);
+    }
+}
