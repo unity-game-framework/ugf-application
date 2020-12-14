e71cc2b5aaa28003f9dbbeca483132fc4a956315
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-14 21:24:04 +0300

    Add ApplicationAccess

---
+++ Packages/UGF.Application/Runtime/ApplicationAccess.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UGF.Application.Runtime
+{
+    public abstract class ApplicationAccess : MonoBehaviour
+    {
+        public IApplication GetApplication()
+        {
+            return OnGetApplication();
+        }
+
+        protected abstract IApplication OnGetApplication();
+    }
+}
