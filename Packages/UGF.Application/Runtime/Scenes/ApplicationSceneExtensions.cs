6b7a1a5e097997f3565d37b1a5ca7fba80748058
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-12-18 19:00:23 +0300

    Add ApplicationSceneExtensions

---
+++ Packages/UGF.Application/Runtime/Scenes/ApplicationSceneExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace UGF.Application.Runtime.Scenes
+{
+    public static class ApplicationSceneExtensions
+    {
+        public static IApplication GetApplication(this Scene scene)
+        {
+            return ApplicationSceneProviderInstance.Provider.Get(scene);
+        }
+
+        public static bool TryGetApplication(this Scene scene, out IApplication application)
+        {
+            return ApplicationSceneProviderInstance.Provider.TryGet(scene, out application);
+        }
+    }
+}
