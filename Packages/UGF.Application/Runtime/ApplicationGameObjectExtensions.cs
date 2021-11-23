f5f09a3feb4547641406fa7331707f3c29a1f6cd
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-11-23 21:26:14 +0300

    Add ApplicationGameObjectExtensions

---
+++ Packages/UGF.Application/Runtime/ApplicationGameObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UGF.RuntimeTools.Runtime.Providers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UGF.Application.Runtime
+{
+    public static class ApplicationGameObjectExtensions
+    {
+        public static IApplication GetApplication(this GameObject gameObject)
+        {
+            return TryGetApplication(gameObject, out IApplication application) ? application : throw new ArgumentException($"Application not found by the specified gameobject: '{gameObject}'.");
+        }
+
+        public static bool TryGetApplication(this GameObject gameObject, out IApplication application)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+
+            var provider = ProviderInstance.Get<IProvider<Scene, IApplication>>();
+
+            return provider.TryGet(gameObject.scene, out application);
+        }
+    }
+}
