110d21548a1b0e32f3a46d35a3f2fcec9f0761ac
Vorobey <vorobyovvorobyov@gmail.com>
2022-11-19 21:59:24 +0100

    Add ApplicationConfigStep

---
+++ Packages/UGF.Application/Editor/Build/ApplicationConfigStep.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UGF.Application.Runtime;
+using UGF.Build.Editor;
+using UGF.Logs.Runtime;
+using UGF.RuntimeTools.Runtime.Contexts;
+
+namespace UGF.Application.Editor.Build
+{
+    public class ApplicationConfigStep : BuildStep
+    {
+        public ApplicationConfigAsset Config { get; }
+
+        public ApplicationConfigStep(ApplicationConfigAsset config)
+        {
+            Config = config ? config : throw new ArgumentNullException(nameof(config));
+        }
+
+        protected override void OnExecute(IBuildSetup setup, IContext context)
+        {
+            ApplicationSettings.Config = Config;
+
+            Log.Info("Application config for project set", new { Config });
+        }
+    }
+}
---
+++ Packages/UGF.Application/Editor/Build/ApplicationConfigStepAsset.cs
@@ -0,0 +1,19 @@
+﻿using UGF.Application.Runtime;
+using UGF.Build.Editor;
+using UnityEngine;
+
+namespace UGF.Application.Editor.Build
+{
+    [CreateAssetMenu(menuName = "Unity Game Framework/Application/Application Config Step", order = 2000)]
+    public class ApplicationConfigStepAsset : BuildStepAsset
+    {
+        [SerializeField] private ApplicationConfigAsset m_config;
+
+        public ApplicationConfigAsset Config { get { return m_config; } set { m_config = value; } }
+
+        protected override IBuildStep OnBuild()
+        {
+            return new ApplicationConfigStep(m_config);
+        }
+    }
+}
