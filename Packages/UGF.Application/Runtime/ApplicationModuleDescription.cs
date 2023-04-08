780a52578eb7c44117e9384382d3d7e65b70f8cd
Vorobey <vorobyovvorobyov@gmail.com>
2023-04-08 14:02:37 +0200

    Update ApplicationModuleDescription.cs

--- Packages/UGF.Application/Runtime/ApplicationModuleDescription.cs
+++ Packages/UGF.Application/Runtime/ApplicationModuleDescription.cs
@@ -5,5 +5,14 @@ namespace UGF.Application.Runtime
     public class ApplicationModuleDescription : IApplicationModuleDescription
     {
         public Type RegisterType { get; set; }
+
+        public ApplicationModuleDescription()
+        {
+        }
+
+        public ApplicationModuleDescription(Type registerType)
+        {
+            RegisterType = registerType ?? throw new ArgumentNullException(nameof(registerType));
+        }
     }
 }
